331911e94c12134b978f2c07999ea5fc4d377a96
DeklanBeckett <juandausuga16@gmail.com>
2024-07-29 15:23:26 -0500

    se crea base del proyecto con conexion a la base de datos

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using SeverBooks.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+//Conexión
+builder.Services.AddDbContext<DataContext> (options => 
+                            options.UseMySql(
+                                builder.Configuration.GetConnectionString("MySqlConnection"),
+                                Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql")));
+
+// CORS
+builder.Services.AddCors(options =>
+    options.AddPolicy("politica", service => {
+        service.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    })
+);                           
+
+
+
+var app = builder.Build();
+
+app.MapControllers();
+app.UseCors("politica");
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+
+app.Run();
+
