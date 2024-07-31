003f92bf140799bfadd850c486ef94bf0fb042bd
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-07-30 20:00:58 -0500

    Se realiza integracion

--- Services/Interfaces/IMailRepository.cs
+++ Services/Interfaces/IMailRepository.cs
@@ -5,5 +5,12 @@ namespace Server_Books.Services
     public interface IMailRepository
     {
         void SendEmailCreateUser(string email, string subject, string body, User user);
+        void SendEmailLogin(string email, string subject, string body, User user);
+        void SendEmailLoanRequest(string email, string subject, string body, BookLending prestamo);
+        void SendEmailLoanApproved(string email, string subject, string body, User user);
+        void SendEmailDueDateReminder(string email, string subject, string body, User user, DateTime dueDate);
+        void SendEmailLoanRequestAdmin(string email, string subject, string body, User user);
     }
 }
+
+
